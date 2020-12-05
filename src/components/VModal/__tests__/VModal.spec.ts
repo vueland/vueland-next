b90dfdbe70ff351175122f6e55c37d80e3e061ef
vueland <vueland.library@gmail.com>
2020-12-05 13:18:08 +0300

    correct test

--- src/components/VModal/__tests__/VModal.spec.ts
+++ src/components/VModal/__tests__/VModal.spec.ts
@@ -1,6 +1,6 @@
 import { mount, VueWrapper } from '@vue/test-utils'
-
 import { VModal } from '../VModal'
+import 'regenerator-runtime/runtime'
 
 describe('VModal', () => {
   let mountFunction: (options?: object) => VueWrapper<any>
@@ -20,6 +20,16 @@ describe('VModal', () => {
     const cmp = mountFunction({ props })
 
     expect(cmp.props().overlay).toBe(true)
+    expect(cmp.find('.v-overlay').exists()).toBe(true)
+    expect(cmp.html()).toMatchSnapshot()
+  })
+
+  it('should set overlay color and match snapshot', () => {
+    const overlayColor = 'red darken-2'
+    const props = { overlayColor, overlay: true }
+    const cmp = mountFunction({ props })
+
+    expect(cmp.find('.v-overlay').attributes().class).toContain(overlayColor)
     expect(cmp.html()).toMatchSnapshot()
   })
 
@@ -30,5 +40,17 @@ describe('VModal', () => {
 
     expect(cmp.props().transition).toBe(transition)
     expect(cmp.html()).toMatchSnapshot()
+  })
+
+  it('should set modelValue prop to true and match snapshot', async () => {
+    const modelValue = true
+    const props = { modelValue, overlay: true }
+    const cmp = mountFunction({ props })
+
+    // Because the overlay component show and hide
+    // on next tick
+    await new Promise(res => setTimeout(res))
+
+    expect(cmp.find('.v-overlay').attributes().class).toContain('v-overlay--active')
   })
 })

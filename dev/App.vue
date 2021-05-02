4d9a7289c4b41c521e335edb035d993bcf639818
vueland <vueland.library@gmail.com>
2021-05-03 02:09:40 +0300

    FIX: data table footer options

--- dev/App.vue
+++ dev/App.vue
@@ -1,126 +1,126 @@
 <script lang="ts">
-  import { reactive, ref, computed, watch } from 'vue'
-  import { FaIcons } from '../src/services/icons'
+import { reactive, ref, computed, watch } from 'vue'
+import { FaIcons } from '../src/services/icons'
 
-  export default {
-    setup() {
-      const data = reactive({
-        always: true,
-        show: false,
-        test: true,
-        login: '',
-        email: '',
-        password: '',
-        user: '',
-        checked: false,
-        user2: { name: 'igor' },
-        date: null,
-        users: [],
+export default {
+  setup() {
+    const data = reactive({
+      always: true,
+      show: false,
+      test: true,
+      login: '',
+      email: '',
+      password: '',
+      user: '',
+      checked: false,
+      user2: { name: 'igor' },
+      date: null,
+      users: []
+    })
+
+    const fetchItems = () => {
+      fetch('https://jsonplaceholder.typicode.com/comments')
+        .then(response => response.json())
+        .then(json => data.users = [...data.users, ...json])
+    }
+
+    fetchItems()
+
+    setTimeout(() => {
+      data.always = false
+    }, 2000)
+
+    watch(
+      () => data.user,
+      to => {
+        console.log(to, 'watch')
+      }, { immediate: true }
+    )
+
+    const toggleAlways = () => {
+      data.always = !data.always
+    }
+
+    const testFunc = date => {
+      console.log(date)
+    }
+
+    const forOut = computed(() => {
+      return data.always ? testFunc : undefined
+    })
+
+    const cols = ref([
+      {
+        key: 'actions',
+        title: 'Actions',
+        align: 'center'
+      },
+      {
+        key: 'name',
+        title: 'Name',
+        width: '250',
+        resizeable: true,
+        sortable: true,
+        filterable: true,
+        cellClass: 'red darken-3',
+        filterClass: 'green darken-3',
+        rowCellClass: 'red lighten-1',
+        format: row => row.name,
+        filter: ({ value, col }) => data.users.filter(user => user[col.key].includes(value))
+      },
+      {
+        key: 'email',
+        title: 'Email',
+        resizeable: true,
+        sortable: true,
+        filterable: true,
+        cellClass: 'green accent-3',
+        filterClass: 'red',
+        rowCellClass: 'green lighten-1',
+        format: row => row.email
+      },
+      {
+        key: 'body',
+        title: 'Body',
+        resizeable: true,
+        sortable: true,
+        filterable: true
+      }
+    ])
+
+    const rows = [
+      {
+        name: 'Ben',
+        email: 'ben@mail.ru',
+        body: 'some body text'
+      },
+      {
+        name: 'Alex',
+        email: 'alex@mail.ru',
+        body: 'some body text'
+      }
+    ]
+
+    const addItem = () => {
+      data.users.push({
+        name: 'Anar',
+        email: 'adsadasdasd',
+        body: 'sdfsddfsdfsdfsf'
       })
+    }
 
-      const fetchItems = () => {
-        fetch('https://jsonplaceholder.typicode.com/comments')
-          .then(response => response.json())
-          .then(json => data.users = [...data.users, ...json])
-      }
-
-      fetchItems()
-
-      setTimeout(() => {
-        data.always = false
-      }, 2000)
-
-      watch(
-        () => data.user,
-        to => {
-          console.log(to, 'watch')
-        }, { immediate: true },
-      )
-
-      const toggleAlways = () => {
-        data.always = !data.always
-      }
-
-      const testFunc = date => {
-        console.log(date)
-      }
-
-      const forOut = computed(() => {
-        return data.always ? testFunc : undefined
-      })
-
-      const cols = ref([
-        {
-          key: 'actions',
-          title: 'Actions',
-          align: 'center',
-        },
-        {
-          key: 'name',
-          title: 'Name',
-          width: '250',
-          resizeable: true,
-          sortable: true,
-          filterable: true,
-          cellClass: 'red darken-3',
-          filterClass: 'green darken-3',
-          rowCellClass: 'red lighten-1',
-          format: row => row.name,
-          filter: ({ value, col }) => data.users.filter(user => user[col.key].includes(value)),
-        },
-        {
-          key: 'email',
-          title: 'Email',
-          resizeable: true,
-          sortable: true,
-          filterable: true,
-          cellClass: 'green accent-3',
-          filterClass: 'red',
-          rowCellClass: 'green lighten-1',
-          format: row => row.email,
-        },
-        {
-          key: 'body',
-          title: 'Body',
-          resizeable: true,
-          sortable: true,
-          filterable: true,
-        },
-      ])
-
-      const rows = [
-        {
-          name: 'Ben',
-          email: 'ben@mail.ru',
-          body: 'some body text',
-        },
-        {
-          name: 'Alex',
-          email: 'alex@mail.ru',
-          body: 'some body text',
-        },
-      ]
-
-      const addItem = () => {
-        data.users.push({
-          name: 'Anar',
-          email: 'adsadasdasd',
-          body: 'sdfsddfsdfsdfsf',
-        })
-      }
-
-      return {
-        data,
-        cols,
-        addItem,
-        testFunc,
-        toggleAlways,
-        fetchItems,
-        forOut,
-        FaIcons,
-      }
-    },
+    return {
+      data,
+      cols,
+      addItem,
+      testFunc,
+      toggleAlways,
+      fetchItems,
+      forOut,
+      FaIcons
+    }
   }
+}
 </script>
 
 <template>
@@ -138,9 +138,9 @@
       <v-list-item @click.stop="testFunc">
         salam
       </v-list-item>
-      <v-text-field label="Подъезд"/>
-      <v-text-field label="Этаж"/>
-      <v-text-field label="Домофон"/>
+      <v-text-field label="Подъезд" />
+      <v-text-field label="Этаж" />
+      <v-text-field label="Домофон" />
     </v-list-group>
   </v-list>
   <v-checkbox
@@ -149,19 +149,30 @@
   />
   <div
     class="table-wrap"
-    style="height: 600px;"
+    style="height: 850px;"
   >
     <v-data-table
       :cols="cols"
       :rows="data.users"
       :rows-on-table="[25, 40, 50, 75]"
-      :header-props="{contentColor: 'grey darken-3', color: 'grey lighten-1', dark: true}"
+      :header-props="{
+        contentColor: 'grey darken-3',
+        color: 'grey lighten-1',
+        dark: true
+      }"
+      :footer-props="{
+        paginationButtonsColor: 'red darken-3',
+        paginationText: 'Кол-во строк',
+      }"
       align="left"
       numbered
       checkbox
       @checked="testFunc"
       @last-page="testFunc"
     >
+      <template #paginationDisplay="{start, last, length}">
+        <span>{{ start + '-' + last + ' из ' + length + ' строк' }}</span>
+      </template>
       <template #actions>
         <v-button
           width="42"
@@ -249,10 +260,10 @@
       v-if="!data.show"
       elevation="5"
     >
-      <v-resize right/>
-      <v-resize bottom/>
-      <v-resize top/>
-      <v-resize left/>
+      <v-resize right />
+      <v-resize bottom />
+      <v-resize top />
+      <v-resize left />
       <v-card-title>
         <span
           style="display: block; width: 55px; height: 55px; border-radius: 50px"
@@ -309,7 +320,7 @@
     >
       <v-card-title>
         <span style="">Testting header</span>
-        <v-checkbox label="test"/>
+        <v-checkbox label="test" />
       </v-card-title>
       <v-card-content>
         <span style="display: block">some little text</span>
@@ -369,56 +380,56 @@
 </template>
 
 <style lang="scss">
-  .active-class {
-    background: #272727;
-    color: white !important;
-  }
+.active-class {
+  background: #272727;
+  color: white !important;
+}
 
-  .wrap {
-    position: absolute;
-    left: 60px;
-    top: 60px;
-    width: calc(100% - 60px);
-    height: calc(100vh - 60px);
-  }
+.wrap {
+  position: absolute;
+  left: 60px;
+  top: 60px;
+  width: calc(100% - 60px);
+  height: calc(100vh - 60px);
+}
 
-  .app {
-    &-header {
-      width: 100%;
-      height: 60px;
-      background: #272727;
-    }
-
-    &-sidebar {
-      position: absolute;
-      top: 60px;
-      left: 0;
-      width: 60px;
-      height: calc(100vh - 60px);
-      background: #272727;
-    }
-  }
-
-  .text {
-    display: inline-block;
-  }
-
-  .test {
-    display: flex;
-    justify-content: center;
+.app {
+  &-header {
     width: 100%;
+    height: 60px;
+    background: #272727;
   }
 
-  .v-data-table {
-    background-color: yellow;
+  &-sidebar {
+    position: absolute;
+    top: 60px;
+    left: 0;
+    width: 60px;
+    height: calc(100vh - 60px);
+    background: #272727;
   }
+}
 
-  .v-data-table > .v-data-table__inner > .v-data-table__header {
-    background-color: #0D47A1 !important;
-    border-color: red !important;
-  }
+.text {
+  display: inline-block;
+}
 
-  body {
-    font-family: Bitstream Charter, sans-serif !important;
-  }
+.test {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+
+.v-data-table {
+  background-color: yellow;
+}
+
+.v-data-table > .v-data-table__inner > .v-data-table__header {
+  background-color: #0D47A1 !important;
+  border-color: red !important;
+}
+
+body {
+  font-family: Bitstream Charter, sans-serif !important;
+}
 </style>

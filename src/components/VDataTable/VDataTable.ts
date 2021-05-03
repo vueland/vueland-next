d8322911b943fc5d394a5c0f6b5ad03e7501e36b
Anar <wiseadme@gmail.com>
2021-05-03 18:18:31 +0300

    data table tests

--- src/components/VDataTable/VDataTable.ts
+++ src/components/VDataTable/VDataTable.ts
@@ -159,25 +159,19 @@ export const VDataTable = defineComponent({
       if (!value && filters[col.key]) {
         delete filters[col.key]
       }
-
       if (value) {
         filters[col.key] = value
       }
-
       if (col.filter) {
         return (data.rows = col.filter({ value, col }))
       }
-
       if (props.customFilter) {
         return props.customFilter(filters as any)
       }
-
       if (!Object.keys(filters).length) {
         return (data.rows = props.rows)
       }
-
       data.rows = filterRows(props.rows, props.cols)
-
       data.page = 1
     }
 

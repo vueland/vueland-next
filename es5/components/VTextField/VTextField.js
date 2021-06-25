121d0b3f1f130f0febd797ecb1f542fa616aaec8
vueland <vueland.library@gmail.com>
2021-06-25 23:05:36 +0300

    build

--- es5/components/VTextField/VTextField.js
+++ es5/components/VTextField/VTextField.js
@@ -98,9 +98,9 @@ var VTextField = (0, _vue.defineComponent)({
 
     function onInput(e) {
       state.value = e.target.value;
-      emit('update:modelValue', state.value);
-      emit('update:value', state.value);
-      emit('input', state.value);
+      emit('update:modelValue', e.target.value);
+      emit('update:value', e.target.value);
+      emit('input', e.target.value);
     }
 
     function genInput() {

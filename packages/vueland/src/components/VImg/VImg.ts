fadc0e1d4483de78c97a78acf5559cd2c04a9413
Anar <wiseadme@gmail.com>
2022-11-29 18:01:13 +0300

    start VLazy and VImg

---
+++ packages/vueland/src/components/VImg/VImg.ts
@@ -0,0 +1,33 @@
+import { defineComponent, h } from 'vue'
+
+export const VImg = defineComponent({
+  name: 'v-img',
+  props: {
+    src: {
+      type: String,
+      default: ''
+    },
+    lazySrc: {
+      type: String,
+      default: ''
+    },
+    alt: {
+      type: String,
+      default: ''
+    },
+    lazy: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  setup(props){
+
+    return () => h('img', {
+      class: 'v-img',
+      src: props.lazy ? props.lazySrc : props.src,
+      alt: props.alt,
+      ['data-src']: props.lazy ? props.src : null,
+    })
+  }
+})
---
+++ packages/vueland/src/components/VLazy/VLazy.ts
@@ -0,0 +1,86 @@
+import { defineComponent, h, ref, unref, onMounted } from '@vue/runtime-core'
+import './VLazy.scss'
+
+export const VLazy = defineComponent({
+  props: {
+    tag: {
+      type: String,
+      default: 'div',
+    },
+    rootMargin: {
+      type: String,
+      default: '0px',
+    },
+    threshold: {
+      type: [Number, String],
+      default: 0.1,
+    },
+    transition: {
+      type: String,
+      default: 'fade-in-transition',
+    },
+  },
+
+  setup(props, { slots }) {
+    const LAZY_ATTR = 'data-src'
+    const containerRef = ref<HTMLElement | null>(null)
+
+    const observerOptions = {
+      root: unref(containerRef)!,
+      rootMargin: props.rootMargin,
+      threshold: Number(props.threshold),
+    }
+
+    let observer: IntersectionObserver
+
+    function onTransitionEnd() {
+      this.classList.remove(`${ props.transition }-enter-active`)
+      this.removeEventListener('transitionend', onTransitionEnd)
+    }
+
+    const onEnter = (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+      let duration = 0
+
+      entries.forEach((it: any) => {
+        if (it.isIntersecting) {
+
+          it.target.classList.add(`${ props.transition }-enter-from`)
+
+          requestAnimationFrame(() => {
+            it.target.src = it.target.dataset.src
+            it.target.removeAttribute(LAZY_ATTR)
+            it.target.classList.add(`${ props.transition }-enter-active`)
+          })
+
+          if (!duration) {
+            duration = parseFloat(getComputedStyle(it.target).transitionDuration) * 1000
+          }
+
+          observer.unobserve(it.target)
+
+          setTimeout(() => {
+            it.target.classList.remove(`${ props.transition }-enter-from`)
+            it.target.classList.remove(`${ props.transition }-enter-active`)
+          }, duration)
+
+          it.target.addEventListener('transitionend', onTransitionEnd)
+        }
+      })
+    }
+
+    onMounted(() => {
+      const elems = unref(containerRef)!.querySelectorAll(`*[${ LAZY_ATTR }]`)
+
+      observer = new IntersectionObserver(onEnter, observerOptions)
+
+      elems.forEach((el: any) => {
+        observer.observe(el)
+      })
+    })
+
+    return () => h(props.tag, {
+      class: 'v-lazy',
+      ref: containerRef,
+    }, slots.default?.())
+  },
+})
